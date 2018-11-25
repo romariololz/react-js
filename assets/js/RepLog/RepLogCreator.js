6a118cf8163cc524cc47f5e893deeadcd56f94c3
Romain Helou <helouromain@gmail.com>
2018-11-25 23:28:30 +0100

    20 - Refs

--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -5,6 +5,9 @@ export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
 
+        this.quantityInput = React.createRef();
+        this.itemSelect = React.createRef();
+
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
@@ -12,10 +15,21 @@ export default class RepLogCreator extends Component {
         event.preventDefault();
         const {onNewItemSubmit} = this.props;
 
-        console.log('I love when a good form submits!');
-        console.log(event.target.elements.namedItem('reps').value);
+        const quantityInput = this.quantityInput.current;
+        const itemSelect = this.itemSelect.current;
 
-        onNewItemSubmit('Big Fat Cat', event.target.elements.namedItem('reps').value);
+        console.log('I love when a good form submits!');
+        console.log(quantityInput.value);
+        console.log(itemSelect.options[itemSelect.selectedIndex].value);
+
+
+        onNewItemSubmit(
+            itemSelect.options[itemSelect.selectedIndex].text,
+            quantityInput.value
+        );
+
+        quantityInput.value = '';
+        itemSelect.selectedIndex = 0;
     }
 
     render() {
@@ -27,7 +41,7 @@ export default class RepLogCreator extends Component {
                         What did you lift?
                     </label>
                     <select id="rep_log_item"
-                            name="item"
+                            ref={this.itemSelect}
                             required="required"
                             className="form-control">
                         <option value="">What did you
@@ -46,7 +60,8 @@ export default class RepLogCreator extends Component {
                         How many times?
                     </label>
                     <input type="number" id="rep_log_reps"
-                           name="reps" required="required"
+                           ref={this.quantityInput}
+                           required="required"
                            placeholder="How many times?"
                            className="form-control"/>
                 </div>
