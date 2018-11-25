2a67941e9a1a70732462b874dc292dd2251b6fa7
Romain Helou <helouromain@gmail.com>
2018-11-25 17:53:27 +0100

    2 - React.createElement()
    Install React:
    	- yarn add react react-dom --dev

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,13 @@
-console.log('Oh hallo React peeps! 🏋️');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift History!',
+    React.createElement('span', null, '❤️')
+);
+
+console.log(el);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
